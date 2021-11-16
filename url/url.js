f556e8993e50247039697a46444d7de897cc2a8c
Dlunhappy226 <76965487+Dlunhappy226@users.noreply.github.com>
2021-11-15 19:13:57 -0800

    url

--- url/url.js
+++ url/url.js
@@ -1,3 +1,6 @@
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+
 var re_onload = function(){
     grecaptcha.render("recap", {
         "sitekey" : "6LdwvTgdAAAAAHOg1zDWhL5Ouci5ddslFp0i1VZC"
@@ -5,9 +8,8 @@ var re_onload = function(){
 };
 
 setInterval(function(){
-    console.log("check")
     var response = grecaptcha.getResponse();
     if(response.length != 0){
-        console.log("yes");
+        window.location.href = urlParams.get("url");
     }
 },1);
